3d9758c79f1bcefb810e64e0d01a1949f4fbc819
Alioune56 <aliounembp3k@gmail.com>
2024-04-03 02:04:50 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 // first method to make a route
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 // second method to make a route
 // Route::view('/home','home');
